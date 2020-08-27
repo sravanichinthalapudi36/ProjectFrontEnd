a4d6d362b21f27756f6d85f878870f130a9d8959
sravani.chinthalapudi@zensar.com <sravani.chinthalapudi@zensar.com>
2020-08-27 18:06:07 +0530

    FrontEnd Pages

---
+++ Project/Project/Controllers/DetailsController.cs
@@ -0,0 +1,35 @@
+﻿using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class DetailsController : Controller
+    {
+        // GET: Details
+        public ActionResult Index()
+        {
+            var Items = GetItems();
+            return View(Items);
+        }
+        private IEnumerable<Items> GetItems()
+        {
+            return new List<Items>{
+               new Items{selectedamount=1,name="Apple Pie",price=1275,totolprice=1275},
+               new Items{selectedamount=1,name="Pumpkin Pie",price=3075,totolprice=3075},
+                new Items{selectedamount=1,name="RhubarbPie",price=1275,totolprice=1275}
+            };
+        }
+        public ActionResult Details(int selectedamount)
+        {
+            var Items = GetItems().SingleOrDefault(d => d.selectedamount == selectedamount);
+            if (Items == null)
+                return HttpNotFound();
+            return View(Items);
+
+        }
+    }
+}
---
+++ Project/Project/Models/Items.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.Models
+{
+    public class Items
+    {
+        public int selectedamount { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public int totolprice { get; set; }
+    }
+}
